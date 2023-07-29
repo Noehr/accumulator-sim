d39e1f3605fd01e2fa686398adf7ddf013ed2f4d
HRAND5 <asbjorn@dissing.nu>
2023-07-29 16:25:42 +0200

    Change to stdint types and load instructions from binary file.

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <stdint.h>
 
 // Enum for ALU operations 
 typedef enum {
@@ -15,7 +15,7 @@ typedef enum {
 } ALUOP;
 
 
-unsigned char alu(ALUOP opcode, unsigned char acc, unsigned char aux) {
+uint8_t alu(ALUOP opcode, uint8_t acc, uint8_t aux) {
 	// Returns next value of accumulator  
 	switch (opcode) {
 	case ALU_NOP:
@@ -40,32 +40,32 @@ unsigned char alu(ALUOP opcode, unsigned char acc, unsigned char aux) {
 }
 
 
-void load(unsigned char * acc, unsigned char * reg, unsigned char address) {
+void load(uint8_t * acc, uint8_t * reg, uint8_t address) {
 	// Copy value at register address into accumulator
 	*acc = *(reg + address);
 }
 
 
-void store(unsigned char * acc, unsigned char * reg, unsigned char address) {
+void store(uint8_t * acc, uint8_t * reg, uint8_t address) {
 	// Copy value from accumulator to value at register address
 	// Pointer to acc is not necessary but is pointer instead of value for consistency
 	*(reg + address) = *acc;
 }
 
 
-void jump(unsigned int * pc, unsigned int new_pc) {
+void jump(uint16_t * pc, uint16_t new_pc) {
 	// Set program counter to new instruction number. 
 	*pc = new_pc;
 }
 
-void branch_zero(unsigned char acc, unsigned int * pc, unsigned int new_pc) {
+void branch_zero(uint8_t acc, uint16_t * pc, uint16_t new_pc) {
 	// Jump to instruction if accumulator is zero
 	if (acc == 0) {
 		jump(pc, new_pc);
 	}
 }
 
-void branch_positive(unsigned char acc, unsigned int * pc, unsigned int new_pc) {
+void branch_positive(uint8_t acc, uint16_t * pc, uint16_t new_pc) {
 	// Jump to instruction if accumulator is positive (>0)
 	// Assumes accumulator is non zero integer. Handling negative number must be implemented in program.
 	if (acc > 0) {
@@ -75,10 +75,18 @@ void branch_positive(unsigned char acc, unsigned int * pc, unsigned int new_pc) 
 
 int main() {
 	// Initialise accumulator, registers and program counter. 
-	unsigned char acc = 0;
-	unsigned char * reg = calloc(255, sizeof(unsigned char));
-	unsigned int pc = 0;
+
+	uint8_t acc = 0;
+	uint8_t * reg = calloc(255, sizeof(uint8_t));
+	uint16_t pc = 0;
+
+	// Initialise and load instruction memory
+	uint16_t * instructions = calloc(4096, sizeof(uint16_t));
 	
+	FILE * file;
+	file = fopen("instructions.bin", "rb");
+	fread(instructions, sizeof(uint16_t), 4096, file);
+	fclose(file);
 
 	return 0;
 }
